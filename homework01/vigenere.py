dd1c6cf241eee88dd13af6c1053c7dba8d091a7c
anton-logom <anton-logom@yndex.ru>
2018-03-29 20:34:34 +0300

    add vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,21 +1,42 @@
 def encrypt_vigenere(plaintext, keyword):
     """
-    Encrypts plaintext using a Vigenere cipher.
-
+    #  Encrypts plaintext using a Vigenere cipher.
+    #
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
     'python'
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
+    >>> encrypt_vigenere("attackatdawn", "lemon")
+    'lxfopvefrnhr'
     """
-    # PUT YOUR CODE HERE
+
+    keyword *= len(plaintext) // len(keyword) + 1
+    ciphertext = ''
+    for i in range(len(plaintext)):
+        upreg = 0
+        s = plaintext[i]
+        k = keyword[i]
+        if ord(s) in range(65, 90):
+            upreg = 1
+        s = s.lower()
+        k = k.lower()
+        if ord(s) in range(97, 122):
+            if (ord(s) + ord(k) - 97) <= 122:
+                s = chr(ord(s) + ord(k) - 97)
+            else:
+                s = chr(ord(s) + ord(k) - 97 - 26)
+        if upreg:
+            s = s.upper()
+        ciphertext += s
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext, keyword):
     """
-    Decrypts a ciphertext using a Vigenere cipher.
+    # Decrypts a ciphertext using a Vigenere cipher.
 
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
@@ -24,5 +45,24 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    # PUT YOUR CODE HERE
+
+    plaintext = ''
+    keyword *= len(ciphertext) // len(keyword) + 1
+    for i in range(len(ciphertext)):
+        upreg = 0
+        s = ciphertext[i]
+        k = keyword[i]
+        if ord(s) in range(65, 90):
+            upreg = 1
+        s = s.lower()
+        k = k.lower()
+        if ord(s) in range(97, 122):
+            if (ord(k) - ord(s)) <= 0:
+                s = chr(ord(s) - ord(k) + 97)
+            else:
+                s = chr(ord(s) - ord(k) + 97 + 26)
+        if upreg:
+            s = s.upper()
+        plaintext += s
+
     return plaintext
